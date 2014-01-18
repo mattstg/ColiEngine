22a6a50885f866b78cd0abf528d7f976547ed568
mattstg <mattstger@gmail.com>
2014-01-18 16:41:16 -0500

    Alot
    Node Iterator called NodeSpider
    GUI world
    Tons of hashtable modifications for inf loop add/sub bugs
    Pretty big overhaul of most classes

---
+++ EntitySystem/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ColiSys;  
+
+namespace EntitySys
+{
+    class Entity
+    {
+        S_XY size;
+        S_XY loc;
+        Node sizeLoc;
+
+
+        public Entity(S_XY tloc,S_XY tsize) { ForceCnstr(tloc,tsize); }
+
+
+        public void ForceCnstr(S_XY tloc,S_XY tsize)
+        {
+            size = tsize;
+            loc = tloc;
+            _SetSizeInNodeForm();
+        }
+
+        private void _SetSizeInNodeForm()
+        {
+            Node temp = new Node(loc.x, loc.x + size.x);
+            temp.Dwn(new Node(loc.y - size.y, loc.y));
+            sizeLoc = temp;
+        }
+
+        public Node RetSizeLocCopy()
+        {
+            return sizeLoc.CopySelf(copyTypes.copyDwn);
+        }
+
+    }
+}
---
+++ SpriteHTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;   //   for Texture2D
+using Microsoft.Xna.Framework; 
+
+
+namespace ColiSys
+{
+    class SpriteHTable
+    {
+        Texture2D t;
+
+
+        public SpriteHTable()
+        { }
+
+
+        public void LoadTexture(Texture2D tt)
+        {
+            t = tt;
+        }
+
+
+        public void Draw(SpriteBatch sb, Node ht,Color color)
+        {
+            if(ht != null)
+            {
+                S_Box range;
+            
+                Node htx = ht.CopySelf(copyTypes.copyBoth) ;
+                Node hty;
+
+                while (htx != null)
+                {
+                    hty = htx.Dwn();
+                    while (hty != null)
+                    {
+                        range = new S_Box(htx.Ret(Bounds.l) * Constants.GAME_SCALE.x, hty.Ret(Bounds.l) * Constants.GAME_SCALE.y, (htx.Ret(Bounds.u)+1) * Constants.GAME_SCALE.x, (hty.Ret(Bounds.u)+1) * Constants.GAME_SCALE.y, false); //guess i could just make it straigh to rectangle eh?
+                        sb.Draw(t, Converter.SBox2Rect(range), color);
+                        //sb.Draw(t, new Rectangle(0,0,100,100), Color.White);
+                        hty = hty.Adj();
+                    }
+                    htx = htx.Adj();
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
