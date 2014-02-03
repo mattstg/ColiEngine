7c80f114ad2ed336ca7bc46ae752cb1d93a49d8b
mikebjoyce <mikebjoyce@gmail.com>
2014-02-03 15:28:27 -0500

    Completed DiagMotion
    Hey Matt, I believe that I have completed DiagMotion. I haven't tested,
    but conceptually it should work... if I made no typos or logical slips.
    It also looks a lot cleaner. :)

--- DiagMotion.cs
+++ DiagMotion.cs
@@ -8,14 +8,23 @@ namespace ColiSys
     { //diagonal motion
         private int x, y;
         public S_Box E;
-        int counter = 0;
-        int Rem;
-        double dR;
-        double CdR;
-        int diagSteps;
-        int RemainderCompleted = 0;
+        int TotalSteps;
+        int CurrentStep = 0;
+
+        //these two variables are used to calculate when we should be doing diagonal movements and non diagonal moveements
+        int DM;  //total diagonal motions
+        int nDM; //total non diag motions
+        
+        int Rem; //remainder to add after all our most frequent motion
+        double dR; //ideal distance (in blocks) between each time we are adding a remainder
+        double CdR; //counter which keeps track of the location we are at in our motion, and increments by dR.
+        int RemainderCompleted = 0; //remainder completed
+        
         new S_Box lastLoc;
 
+        enum MainMotion{ Diago, notDiag, Mixed };
+        MainMotion thisMotion;
+       
         //constructors
         public DiagMotion(int a, int b, S_Box tE)
         {
@@ -28,40 +37,57 @@ namespace ColiSys
 
         public S_Box NodetoBox(Node tN)
         {
-            E = new S_Box(tN.Ret(Bounds.l), tN.Ret(Bounds.u), tN.Dwn().Ret(Bounds.l), tN.Dwn().Ret(Bounds.u), true);
+            E = new S_Box(tN.Ret(Bounds.l), tN.Dwn().Ret(Bounds.u), tN.Ret(Bounds.u) - tN.Ret(Bounds.l) + 1, tN.Dwn().Ret(Bounds.u) - tN.Dwn().Ret(Bounds.l) + 1, true);
             return E;
         }
 
         private void priCalc(){
-            if (Math.Abs(x) > Math.Abs(y))
-            {
-                Rem = Math.Abs(x) - Math.Abs(y);
-                dR = (double)Math.Abs(y) / (double)Rem;
+            TotalSteps = (Math.Abs(x) > Math.Abs(y))? Math.Abs(x): Math.Abs(y);
+            if(Math.Abs(x) > Math.Abs(y)){ //if x velocity is largest 
+                nDM = Math.Abs(x) - Math.Abs(y); // non diagonal motions
+                DM = nDM - TotalSteps; //diag motions being deduces
+            }else{
+                nDM = Math.Abs(y) - Math.Abs(x); // non diagonal motions
+                DM = nDM - TotalSteps; //diag motions being deduces
             }
-            else if (Math.Abs(x) < Math.Abs(y))
+
+            if (DM != nDM)
             {
-                Rem = Math.Abs(y) - Math.Abs(x);
-                dR = (double)Math.Abs(x) / (double)Rem;
+                thisMotion = (DM > nDM) ? MainMotion.Diago : MainMotion.notDiag;
             }
             else
             {
-                Rem = 0;
-                dR = 0;
+                thisMotion = MainMotion.Mixed;
             }
+
+
+            switch (thisMotion)
+            {
+                case MainMotion.Mixed: //so in the case where there is equal number of DM and nDM we need to arbitrarily choose one... So I made it the same as Equal.
+                case MainMotion.Diago:
+                    //remainder will be the nDM
+                    Rem = nDM;
+                    //dR should be the spacing between the nDM
+                    dR = TotalSteps / DM;
+                    //CdR should be counting up until the next moment where a nDM should be added.
+                    break;
+
+                case MainMotion.notDiag:
+                    //remainder will be the DM
+                    Rem = DM;
+                    //dR should be the spacing between the DM
+                    dR = TotalSteps / nDM;
+                    //CdR should be counting up until the next moment where a DM should be added.
+                    break;
+                default:
+                    break;
+            };
             CdR = dR / 2;
-            if (x == 0 || y == 0)
-            {
-                diagSteps = (Math.Abs(x) > Math.Abs(y)) ? Math.Abs(x) : Math.Abs(y);
-            }
-            else
-            {
-                diagSteps = (Math.Abs(x) > Math.Abs(y)) ? Math.Abs(y) : Math.Abs(x);
-            }
         }
 
         public DiagMotion(int a, int b, Node tN)
         {
-            E = new S_Box(tN.Ret(Bounds.l), tN.Ret(Bounds.u), tN.Dwn().Ret(Bounds.l), tN.Dwn().Ret(Bounds.u), true);
+            E = NodetoBox(tN); 
             lastLoc = new S_Box(E);
             x = a;
             y = b;
@@ -75,14 +101,13 @@ namespace ColiSys
             Rem = 0;
             dR = 0;
             CdR = 0;
-            diagSteps = 1;
+            TotalSteps = 1;
         }
 
         public string GenString()
         {
             return "velocity: [" + x + " , " + y + "]";
         }
-
 
         //this returns a Box object which tell us the next location which we need to check
         private S_Box VelToBox()
@@ -109,6 +134,30 @@ namespace ColiSys
             S_Box i = new S_Box(E.loc.x + a, E.loc.y + b, E.size.x + c + 1, E.size.y + d + 1);
             return i;
         }
+        private S_Box VelToBox(int xx, int yy)
+        {
+            int a = 0, b = 0, c = 0, d = 0;
+            if (xx < 0)
+            {
+                a = -1;
+            }
+            else if (xx == 0)
+            {
+                c = -1;
+            }
+
+            if (yy > 0)
+            {
+                b = 1;
+            }
+            else if (yy == 0)
+            {
+                d = -1;
+            }
+
+            S_Box i = new S_Box(E.loc.x + a, E.loc.y + b, E.size.x + c + 1, E.size.y + d + 1);
+            return i;
+        }
 
         //this method moves the copy of the object you are collision testing forward one step in its motion.
         private void PMove()
@@ -128,8 +177,8 @@ namespace ColiSys
                 else
                     ty = -1;
             }
-            E.loc.x = E.loc.x + tx;
-            E.loc.y = E.loc.y + ty;
+            E.loc.x =+ tx;
+            E.loc.y =+ ty;
         }
         private void PMove(int xx, int yy)
         {
@@ -154,62 +203,69 @@ namespace ColiSys
 
         public S_Box RetNextBox()
         {
-            if ((counter) >= diagSteps)
+            lastLoc = (CurrentStep != 0) ? E : lastLoc;
+            if ((CurrentStep) >= TotalSteps) { return null; };
+            S_Box i = null; //initializing our colision box
+            if (((double)CurrentStep >= CdR) && (RemainderCompleted != Rem) && (Rem != 0) && (CdR != 0)) //this is the special case we need to be adding periodically 
             {
-                return null;
-            }
-
-            if(counter != 0)
-            {
-                lastLoc = E;
-            }
-
-            S_Box i = VelToBox();
-            
-            if ((double)counter >= CdR && Rem != 0 && CdR != 0 && RemainderCompleted != Rem)
-            {
-                if (x == diagSteps)
+                switch (thisMotion)
                 {
-                    if (y > 0)
-                    {
-                        i.loc.y += 1;
-                        i.size.y += 1;
-                        PMove(0, 1);
-                    }
-                    else
-                    {
-                        i.size.y += 1;
-                        PMove(0, -1);
-                    }
-                }
-                else
-                {
-                    if (x > 0)
-                    {
-                        i.size.x += 1;
-                        PMove(1, 0);
-                    }
-                    else
-                    {
-                        i.loc.x += 1;
-                        i.size.x += 1;
-                        PMove(-1, 0);
-                    }
-                }
-                //Console.Out.WriteLine("E has been moved to: " + E.GenString());
+                    case MainMotion.Mixed:
+                    case MainMotion.Diago: //if there are more diagonal movements than horizontal/virtical ones
+                        if (Math.Abs(x) > Math.Abs(y))
+                        {  //moving in the x
+                            i = VelToBox((Math.Abs(x)/x), 0); //generating coli box for x only motion
+                            PMove((Math.Abs(x)/x),0); //moving phantom 
+                        }
+                        else
+                        { //moving in the y
+                            i = VelToBox(0, (Math.Abs(y)/y)); //generating coli box for y only motion
+                            PMove(0, (Math.Abs(y)/y)); //moving phantom 
+                        }
+                        break;
+                    case MainMotion.notDiag: //if there are more horizontal/virtical movements than diagonal ones
+                        i = VelToBox();
+                        PMove();
+                        break;
+                    default:
+                        break;
+                };
                 CdR += dR;
                 RemainderCompleted++;
             }
-        
-            PMove();
-            counter += 1;
-            return i;
+            else //so this is the normal case... be doing the opposite of the special case.
+            {
+                switch (thisMotion)
+                {
+                    case MainMotion.Mixed:
+                    case MainMotion.Diago: //if there are more diagonal movements than horizontal/virtical ones
+                        i = VelToBox(); //doing a typical diagonal motion
+                        PMove(); //appropriately moving phantom
+                        break;
+                    case MainMotion.notDiag: //if there are more horizontal/virtical movements than diagonal ones
+                       //we then need to be doing a not Diagonal movement 
+                        if (Math.Abs(x) > Math.Abs(y))
+                        {  //moving in the x only
+                            i = VelToBox((Math.Abs(x) / x), 0);
+                            PMove((Math.Abs(x) / x), 0); //appropriately moving phantom
+                        }
+                        else
+                        { //moving in the y only
+                            i = VelToBox(0, (Math.Abs(y) / y));
+                            PMove(0, (Math.Abs(y) / y)); //appropriately moving phantom
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            CurrentStep += 1;
+            return i; //returning colibox
         }
 
         public S_Box RetLast()
         {
-
-            return lastLoc;
+            return lastLoc; //returns the last location of the phantom (E)
         }
 
     }
