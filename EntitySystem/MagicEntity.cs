69c636c137002ccdddfbf9b24ac8369170087564
mattstg <mattstger@gmail.com>
2014-03-19 21:12:34 -0400

    comp swap
    dfghjkl;

---
+++ EntitySystem/MagicEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntSys
+{
+    class MagicEntity : BodyMechanics
+    {
+
+        public MagicEntity()
+        {
+            ForceCnstr(null);
+        }
+
+        public MagicEntity(DNA dna)
+        {
+
+            ForceCnstr(dna);
+        }
+
+        public void ForceCnstr(DNA dna)
+        {
+
+
+        }
+
+        public void Update(float rt)
+        {}
+
+        
+
+
+
+    }
+}
